0ac9f20458940981a07a15f3d380239eb85cdc4e
Karthik Pandurangan <kpandurangan@FORZACONSULTING.local>
2017-07-11 22:48:48 +0200

    initial commit

---
+++ public/dashboard.js
@@ -0,0 +1,475 @@
+var chart5Options = {
+
+    chart: {
+        renderTo: 'container_chart5',
+        zoomType: 'x'
+    },
+
+    title: {
+        text: 'USD to EUR exchange rate over time'
+    },
+    subtitle: {
+        text: document.ontouchstart === undefined ?
+            'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+    },
+    xAxis: {
+        type: 'datetime'
+    },
+    yAxis: {
+        title: {
+            text: 'Exchange rate'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    plotOptions: {
+        area: {
+            fillColor: {
+                linearGradient: {
+                    x1: 0,
+                    y1: 0,
+                    x2: 0,
+                    y2: 1
+                },
+                stops: [
+                    [0, Highcharts.getOptions().colors[0]],
+                    [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                ]
+            },
+            marker: {
+                radius: 2
+            },
+            lineWidth: 1,
+            states: {
+                hover: {
+                    lineWidth: 1
+                }
+            },
+            threshold: null
+        }
+    },
+
+    series: [{
+        type: 'area',
+        name: 'USD to EUR',
+        data: ([
+            [Date.UTC(2015, 4, 24), 10],
+            [Date.UTC(2015, 4, 25), 15],
+            [Date.UTC(2015, 5, 26), 20],
+            [Date.UTC(2015, 5, 27), 10],
+            [Date.UTC(2015, 6, 4), 12],
+            [Date.UTC(2015, 6, 14), 5],
+            [Date.UTC(2015, 7, 20), 10],
+            [Date.UTC(2015, 8, 2), 15],
+            [Date.UTC(2015, 8, 4), 20],
+            [Date.UTC(2015, 9, 4), 10],
+            [Date.UTC(2015, 9, 8), 12],
+            [Date.UTC(2015, 9, 15), 5]
+        ])
+    }]
+}
+
+new Highcharts.chart(chart5Options);
+
+var chart6Options = {
+
+    chart: {
+        renderTo: 'container_chart6',
+        type: 'pie'
+    },
+
+    title: {
+        text: 'Browser market shares.'
+    },
+    subtitle: {
+        text: 'Source: netmarketshare.com.'
+    },
+    plotOptions: {
+        series: {
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}: {point.y:.1f}%'
+            }
+        }
+    },
+
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Microsoft Internet Explorer',
+            y: 56.33,
+            drilldown: 'Microsoft Internet Explorer'
+        }, {
+            name: 'Chrome',
+            y: 24.03,
+            drilldown: 'Chrome'
+        }, {
+            name: 'Firefox',
+            y: 10.38,
+            drilldown: 'Firefox'
+        }, {
+            name: 'Safari',
+            y: 4.77,
+            drilldown: 'Safari'
+        }, {
+            name: 'Opera',
+            y: 0.91,
+            drilldown: 'Opera'
+        }, {
+            name: 'Proprietary or Undetectable',
+            y: 0.2,
+            drilldown: null
+        }]
+    }],
+    drilldown: {
+        series: [{
+            name: 'Microsoft Internet Explorer',
+            id: 'Microsoft Internet Explorer',
+            data: [
+                ['v11.0', 24.13],
+                ['v8.0', 17.2],
+                ['v9.0', 8.11],
+                ['v10.0', 5.33],
+                ['v6.0', 1.06],
+                ['v7.0', 0.5]
+            ]
+        }, {
+            name: 'Chrome',
+            id: 'Chrome',
+            data: [
+                ['v40.0', 5],
+                ['v41.0', 4.32],
+                ['v42.0', 3.68],
+                ['v39.0', 2.96],
+                ['v36.0', 2.53],
+                ['v43.0', 1.45],
+                ['v31.0', 1.24],
+                ['v35.0', 0.85],
+                ['v38.0', 0.6],
+                ['v32.0', 0.55],
+                ['v37.0', 0.38],
+                ['v33.0', 0.19],
+                ['v34.0', 0.14],
+                ['v30.0', 0.14]
+            ]
+        }, {
+            name: 'Firefox',
+            id: 'Firefox',
+            data: [
+                ['v35', 2.76],
+                ['v36', 2.32],
+                ['v37', 2.31],
+                ['v34', 1.27],
+                ['v38', 1.02],
+                ['v31', 0.33],
+                ['v33', 0.22],
+                ['v32', 0.15]
+            ]
+        }, {
+            name: 'Safari',
+            id: 'Safari',
+            data: [
+                ['v8.0', 2.56],
+                ['v7.1', 0.77],
+                ['v5.1', 0.42],
+                ['v5.0', 0.3],
+                ['v6.1', 0.29],
+                ['v7.0', 0.26],
+                ['v6.2', 0.17]
+            ]
+        }, {
+            name: 'Opera',
+            id: 'Opera',
+            data: [
+                ['v12.x', 0.34],
+                ['v28', 0.24],
+                ['v27', 0.17],
+                ['v29', 0.16]
+            ]
+        }]
+    }
+}
+
+new Highcharts.chart(chart6Options);
+
+var chart1Options = {
+    chart: {
+        renderTo: 'container_chart1',
+        type: 'bar'
+    },
+    title: {
+        text: 'Item availability 1'
+    },
+    subtitle: {
+        text: 'Source: JDE EnterpriseOne, Forza DV910'
+    },
+    xAxis: {
+        categories: ['Items'],
+        title: {
+            text: ['Item 1', 'Item 2', 'Item 3']
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Availability',
+            align: 'high'
+        },
+        labels: {
+            overflow: 'justify'
+        }
+    },
+    tooltip: {
+        valueSuffix: ' primary UOM'
+    },
+    plotOptions: {
+        bar: {
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'Availability',
+        data: [1, 3, -1]
+    }]
+}
+
+new Highcharts.chart(chart1Options);
+
+var chart2Options = {
+    chart: {
+        renderTo: 'container_chart2',
+        type: 'column',
+        options3d: {
+            enabled: true,
+            alpha: 15,
+            beta: 15,
+            depth: 50,
+            viewDistance: 25
+        }
+    },
+    title: {
+        text: 'Chart rotation demo'
+    },
+    subtitle: {
+        text: 'Test options by dragging the sliders below'
+    },
+    plotOptions: {
+        column: {
+            depth: 25
+        }
+    },
+    series: [{
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+    }]
+}
+
+new Highcharts.chart(chart2Options);
+
+var chart3Options = {
+
+    chart: {
+        renderTo: 'container_chart3',
+        type: 'solidgauge'
+    },
+
+    title: null,
+
+    pane: {
+        center: ['50%', '85%'],
+        size: '140%',
+        startAngle: -90,
+        endAngle: 90,
+        background: {
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+            innerRadius: '60%',
+            outerRadius: '100%',
+            shape: 'arc'
+        }
+    },
+
+    tooltip: {
+        enabled: false
+    },
+
+    // the value axis
+    yAxis: {
+        stops: [
+            [0.1, '#55BF3B'], // green
+            [0.5, '#DDDF0D'], // yellow
+            [0.9, '#DF5353'] // red
+        ],
+        lineWidth: 0,
+        minorTickInterval: null,
+        tickAmount: 2,
+        title: {
+            y: -70
+        },
+        labels: {
+            y: 16
+        }
+    },
+
+    plotOptions: {
+        solidgauge: {
+            dataLabels: {
+                y: 5,
+                borderWidth: 0,
+                useHTML: true
+            }
+        }
+    }
+}
+
+var chartSpeed = new Highcharts.chart(Highcharts.merge(chart3Options, {
+    yAxis: {
+        min: 0,
+        max: 200,
+        title: {
+            text: 'Speed'
+        }
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'Speed',
+        data: [80],
+        dataLabels: {
+            format: '<div style="text-align:center"><span style="font-size:15px;color:' +
+            ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
+            '<span style="font-size:12px;color:silver">km/h</span></div>'
+        },
+        tooltip: {
+            valueSuffix: ' km/h'
+        }
+    }]
+
+}));
+
+var chart4Options = {
+
+    chart: {
+        renderTo: 'container_chart4',
+        type: 'solidgauge'
+    },
+
+    title: null,
+
+    pane: {
+        center: ['50%', '85%'],
+        size: '140%',
+        startAngle: -90,
+        endAngle: 90,
+        background: {
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+            innerRadius: '60%',
+            outerRadius: '100%',
+            shape: 'arc'
+        }
+    },
+
+    tooltip: {
+        enabled: false
+    },
+
+    // the value axis
+    yAxis: {
+        stops: [
+            [0.1, '#55BF3B'], // green
+            [0.5, '#DDDF0D'], // yellow
+            [0.9, '#DF5353'] // red
+        ],
+        lineWidth: 0,
+        minorTickInterval: null,
+        tickAmount: 2,
+        title: {
+            y: -70
+        },
+        labels: {
+            y: 16
+        }
+    },
+
+    plotOptions: {
+        solidgauge: {
+            dataLabels: {
+                y: 5,
+                borderWidth: 0,
+                useHTML: true
+            }
+        }
+    }
+}
+
+var chartSpeed2 = new Highcharts.chart(Highcharts.merge(chart4Options, {
+    yAxis: {
+        min: 0,
+        max: 200,
+        title: {
+            text: 'Speed'
+        }
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'Speed',
+        data: [80],
+        dataLabels: {
+            format: '<div style="text-align:center"><span style="font-size:15px;color:' +
+            ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
+            '<span style="font-size:12px;color:silver">km/h</span></div>'
+        },
+        tooltip: {
+            valueSuffix: ' km/h'
+        }
+    }]
+
+}));
+
+// Bring life to the dials
+setInterval(function () {
+    // Speed
+    var point,
+        newVal,
+        inc;
+
+    if (chartSpeed) {
+        point = chartSpeed.series[0].points[0];
+        inc = Math.round((Math.random() - 0.5) * 100);
+        newVal = point.y + inc;
+
+        if (newVal < 0 || newVal > 200) {
+            newVal = point.y - inc;
+        }
+
+        point.update(newVal);
+    }
+
+    if (chartSpeed2) {
+        point = chartSpeed2.series[0].points[0];
+        inc = Math.round((Math.random() - 0.5) * 100);
+        newVal = point.y + inc;
+
+        if (newVal < 0 || newVal > 200) {
+            newVal = point.y - inc;
+        }
+
+        point.update(newVal);
+    }
+
+}, 2000);
